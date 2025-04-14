ed4624b1f9453924032df10f9a542047bc2eca4c
Henry <yuiorts09@gmail.com>
2025-04-14 22:32:33 +0800

    [2200013168] model structure

--- README.md
+++ README.md
@@ -2,6 +2,27 @@
 
 The report is to introduce the pretraining of gpt2 model. I will give the detailed explanation of every git commit in the report and I will also illstrate all the important steps of pretraining gpt2 model.
 
-1. **create readme**: This is the first commit, which is to create the readme file.
+## 1. create readme: 
+This is the first commit, which is to create the readme file.
 
-2. **config**: This commit is to create a config class for the gpt2 model, which contains the hyperparameters of the gpt2 structure. Having a config class makes it convenient to change the hyperparameters of the model. 
+## 2. config: 
+This commit is to create a config class for the gpt2 model, which contains the hyperparameters of the gpt2 structure. Having a config class makes it convenient to change the hyperparameters of the model. The config class is in config.py file.
+
+## 3. model structure: 
+This commit is to create the gpt2 model structure. The 124M GPT2 consists of a word token embedding layer(wte), a word position embedding layer(wpe), 12 transformer blocks, a layer normalization layer and a linear layer. Following the  tutorial from Karparthy, I implemented the gpt2 model structure in the following steps:
+
+1. **The overall structure of the gpt2 model**: This is implemented in class GPT2, in the init function, I use nn.Embedding to create wte and wpe, nn.LayerNorm to create ln_f and a nn.Linear layer for the output layer. The only complicated part is the transformer block. I use the nn.ModuleList to create the 12 blocks and leave the block class to be defined in the next step. For the forward function of GPT2 class, I follow Karparthy's tutorial and make the input as a (B, T) tensor. For each row, the T numbers are the index of the tokens in the vocabulary. The forward function converts the index to the corresponding embeddings and processes them through the gpt2 model. The output is then passed through the linear layer to produce the final logits and the logits are fed into the cross entropy loss function if training.
+
+2. **The transformer block**: The transformer block is implemented in the class Block. The Block class contains the following components:
+   - **ln_1**: This is a layer normalization layer implemented using nn.LayerNorm.
+   - **attn**: This is the attention layer, which is implemented in the next step.
+   - **ln_2**: This is another layer normalization layer implemented using nn.LayerNorm.
+   - **mlp**: This is a multi-layer perceptron, which is implemented using nn.Sequential containing two linear layers and a GELU activation function.
+
+For the forward function, it is trivial, if the data is represented as x, then we just need to feed x to the attention layer and the mlp, with layer normalization and residual connections applied to both the outputs.
+
+3. **The attention layer**: The attention layer is implemented in the class CausalAttention. The attention layer is fed with the input x and needs to compute the attention scores and the attention output. The most important thing is how to create the right attention mask. The attention mask is a lower triangular matrix, which won't participate the gradient flow during backpropagation. Because of this, I use the torch.tril function to create the attention mask and put the attention mask into the register_buffer, which is a buffer that won't participate in the gradient flow. The implementation of the forward function is as follows:
+    - First, I compute the attention scores for multiple heads using the query, key and value matrices. The attention scores are computed using the formula: Q * K^T / sqrt(d_k), where Q is the query matrix, K is the key matrix and d_k is the dimension of the key matrix.
+    - Then, I apply the attention mask to the attention scores. Since we have already created a lower triangular matrix, we can simply extract the first T rows and T columns (T is the sequence length) of the attention mask, which is a lower triangular matrix, and then apply the attention mask to the attention scores. It means that the attention scores for the future tokens are set to -inf and it makes sure that the attention scores for the future tokens won't participate in the gradient flow.
+    - Finally, I compute the attention output using the softmax function and the value matrix for every head. The attention output is computed using the formula: softmax(Q * K^T / sqrt(d_k)) * V, where V is the value matrix. The attention output is then concatenated and projected to the original dimension using the linear layer. The output is then returned as the final result of the forward function.
+
---
+++ model.py
@@ -0,0 +1,115 @@
+from config import GPTConfig
+
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class CausalAttention(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
+        # regularization
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.head_dimension = config.n_embd // config.n_head
+        # not really a 'bias', more of a mask, but following the OpenAI/HF naming though
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                                     .view(1, 1, config.block_size, config.block_size))
+
+    def forward(self, x):
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        # nh is "number of heads", hs is "head size", and C (number of channels) = nh * hs
+        # e.g. in GPT-2 (124M), n_head=12, hs=64, so nh*hs=C=768 channels in the Transformer
+        assert C == self.n_embd, f"Input embedding size {C} does not match model embedding size {self.n_embd}."
+
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, self.head_dimension).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, self.head_dimension).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, self.head_dimension).transpose(1, 2) # (B, nh, T, hs)
+        # attention (materializes the large (T,T) matrix for all the queries and keys)
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        # output projection
+        y = self.c_proj(y)
+        return y
+
+class Block(nn.Module):
+    def __init__(self, config: GPTConfig):
+        super().__init__()
+        self.ln1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalAttention(config)
+        self.ln2 = nn.LayerNorm(config.n_embd)
+        self.mlp = nn.Sequential(
+            nn.Linear(config.n_embd, 4 * config.n_embd),
+            nn.GELU(),
+            nn.Linear(4 * config.n_embd, config.n_embd),
+        )
+
+    def forward(self, x):
+        # Layer normalization and attention
+        x = x + self.attn(self.ln1(x))
+        # Layer normalization and MLP
+        x = x + self.mlp(self.ln2(x))
+        return x
+
+
+
+
+class GPT2(nn.Module):
+    def __init__(self, config: GPTConfig):
+        super().__init__()
+        self.config = config
+
+        # transformer
+        self.transformer = nn.ModuleDict(dict(
+            wte=nn.Embedding(config.vocab_size, config.n_embd),  # word token embedding
+            wpe=nn.Embedding(config.block_size, config.n_embd),  # position embedding
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),  # transformer blocks
+            ln_f=nn.LayerNorm(config.n_embd),  # final layer norm
+        ))
+
+        # Final linear layer
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+    def forward(self, idx, targets=None):
+        # idx is of shape (B, T)
+        B, T = idx.size()
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
+        # forward the token and posisition embeddings
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device) # shape (T)
+        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (T, n_embd)
+        tok_emb = self.transformer.wte(idx) # token embeddings of shape (B, T, n_embd)
+        x = tok_emb + pos_emb
+        # forward the blocks of the transformer
+        for block in self.transformer.h:
+            x = block(x)
+        # forward the final layernorm and the classifier
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x) # (B, T, vocab_size)
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits, loss
+
+    def _init_weights(self):
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+            else:
+                nn.init.zeros_(p)
+
